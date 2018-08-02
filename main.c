569bf7029b3defc7b955c5bdc28697967e34d34a
Leon <>
2018-08-02 20:04:23 +0200

    update main.c

--- main.c
+++ main.c
@@ -1,15 +1,35 @@
-Applications
-Creative Cloud Files
-Desktop
-Documents
-Downloads
-Dropbox
-Google Drive
-Library
-Movies
-Music
-Pictures
-Public
-awscli-bundle
-awscli-bundle.zip
-mygit
+total 17096
+drwxr-xr-x+  5 leon  staff      160 Jul  7  2016 Public/
+drwx------+  3 leon  staff       96 Jul  7  2016 Movies/
+drwx------   3 leon  staff       96 Jul 30  2016 Applications/
+drwx------   3 leon  staff       96 Jul 31  2016 .cups/
+-rw-r--r--   1 leon  staff      168 Sep 12  2016 .bash_profile
+drwxr-xr-x   2 leon  staff       64 Sep 14  2016 .matplotlib/
+-r--------   1 leon  staff        7 Oct 23  2016 .CFUserTextEncoding
+drwx------  27 leon  staff      864 Apr 18  2017 .bash_sessions/
+-rw-r--r--   1 leon  staff  8682054 Apr 18  2017 awscli-bundle.zip
+drwxr-xr-x   4 leon  staff      128 Apr 18  2017 awscli-bundle/
+drwxr-xr-x   4 leon  staff      128 Apr 18  2017 .aws/
+drwx------   5 leon  staff      160 May 27  2017 .vnc/
+drwx------@ 12 leon  staff      384 Jul  4  2017 Google Drive/
+drwxr-xr-x   7 root  admin      224 Oct  1  2017 ../
+drwx------  13 leon  staff      416 Mar 28 20:49 .dropbox/
+drwx------@ 19 leon  staff      608 Jun  2 18:02 Dropbox/
+-rw-r--r--   1 leon  staff       27 Jun  8 10:37 .hgignore_global
+-rw-r--r--   1 leon  staff       13 Jun  8 10:37 .gitignore_global
+-rw-------   1 leon  staff     6913 Jun 19 19:33 .bash_history
+drwxrwxr-x@  3 leon  staff       96 Jun 29 19:23 Creative Cloud Files/
+drwx------@ 27 leon  staff      864 Jul  2 20:41 Documents/
+drwx------@ 72 leon  staff     2304 Jul  2 21:34 Library/
+drwx------+  5 leon  staff      160 Jul  3 06:37 Music/
+drwx------   4 leon  staff      128 Jul 20 22:44 .Trash/
+drwx------+ 16 leon  staff      512 Jul 28 10:42 Pictures/
+drwx------+ 29 leon  staff      928 Aug  2 19:27 Downloads/
+drwx------   6 leon  staff      192 Aug  2 19:30 .ssh/
+-rw-r--r--   1 leon  staff      331 Aug  2 19:31 .gitconfig
+-rw-r--r--@  1 leon  staff       82 Aug  2 19:31 .gitflow_export
+drwx------@  5 leon  staff      160 Aug  2 19:39 Desktop/
+-rw-r--r--@  1 leon  staff    28676 Aug  2 19:41 .DS_Store
+drwxr-xr-x+ 34 leon  staff     1088 Aug  2 19:48 ./
+-rw-------   1 leon  staff      888 Aug  2 19:48 .viminfo
+drwxr-xr-x   4 leon  staff      128 Aug  2 19:49 mygit/
