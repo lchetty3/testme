03789b6b01034e73f1146ef59b13b90eed692b74
Leon at Home <leon@LeonChettyMacbook.local>
2018-08-02 19:53:14 +0200

    asdfqwer

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
-# testme
+Applications
+Creative Cloud Files
+Desktop
+Documents
+Downloads
+Dropbox
+Google Drive
+Library
+Movies
+Music
+Pictures
+Public
+awscli-bundle
+awscli-bundle.zip
+mygit
